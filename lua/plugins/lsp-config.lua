0768edf7bb6622e1883555115d69f088437cc203
noah <154337b@acadiau.ca>
2024-08-02 11:32:38 -0300

    init commit

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,43 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "gopls", "html", "cssls", "astro", "tsserver" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.gopls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.cssls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.astro.setup({
+				capabilties = capabilities,
+			})
+
+			lspconfig.tsserver.setup({
+				capabilities = capabilities
+			})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+		end,
+	},
+}
---
+++ lua/plugins/tmux.lua
@@ -0,0 +1,12 @@
+return {
+
+	"christoomey/vim-tmux-navigator",
+	lazy = true,
+	keys = {
+		{"<c-h>", "<cmd>TmuxNavigateLeft<cr>"},
+		{"<c-j>", "<cmd>TmuxNavigateDown<cr>"},
+		{"<c-k>", "<cmd>TmuxNavigateUp<cr>"},
+		{"<c-l>", "<cmd>TmuxNavigateRight<cr>"},
+	},
+
+}
